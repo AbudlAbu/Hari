d7b334d6a51d75d0dd54797e933fb8cfd623737c
Abu I <84893883+AbudlAbu@users.noreply.github.com>
2021-06-12 19:31:44 +0530

    hai

---
+++ src/main/java/com/hcl/customer/AbuNewApplication.java
@@ -0,0 +1,15 @@
+package com.hcl.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbuNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbuNewApplication.class, args);
+		System.out.println("hai hari");
+		
+	}
+
+}
